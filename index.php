4dea1837a3fa0777512250853f070c31a44f5126
domiluk <151018843+domiluk@users.noreply.github.com>
2024-06-12 18:30:58 +0200

    add router

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
 
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$segments = explode('/', $path);
 
-$controller = $segments[1];
-$action = $segments[2];
-// todo: both action and controller: undefined array key if nothing passed
+require 'src/router.php';
+
+$router = new Router;
+$router->add('/home/index', ['controller' => 'home', 'action' => 'index']);
+$router->add('/products', ['controller' => 'products', 'action' => 'index']);
+$router->add('/', ['controller' => 'home', 'action' => 'index']);
+
+$params = $router->match($path);
+
+if ($params === false) {
+  exit("No route matched");
+}
+
+$controller = $params['controller'];
+$action = $params['action'];
 
 require "src/controllers/$controller.php";
 $controller_object = new $controller;
