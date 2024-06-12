bb212533d44079a29bd74326fa3c65abb2f7ce9a
domiluk <151018843+domiluk@users.noreply.github.com>
2024-06-12 18:08:22 +0200

    pretty urls

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$segments = explode('/', $path);
 
-$action = $_GET['action'];
-$controller = $_GET['controller'];
+$controller = $segments[1];
+$action = $segments[2];
 // todo: both action and controller: undefined array key if nothing passed
 
 require "src/controllers/$controller.php";
